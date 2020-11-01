b7a3e3b022da43232e381eb3363841089706cae9
anaudday <audayrit@gmail.com>
2020-10-31 20:32:57 -0700

    add js

--- audiyps/diyps.js
+++ audiyps/diyps.js
@@ -1,19 +1,21 @@
 var img;
-var initials ='ad'; // your initials
+var boba ='mybobatea'; // your initials
 var choice = '1'; // starting choice, so it is not empty
-var screenbg = 250; // off white background
+var screenbg = (199, 209, 255);
 var lastscreenshot=61; // last screenshot never taken
 
 function preload() {
-// preload() runs once, it may make you wait
-//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
-// you can link to an image on your github account
-  img = loadImage('https://dma-git.github.io/images/74.png');
+  glass = loadImage('https://anaudday.github.io/audiyps/empty-glass.png');
+  bobapearl = loadImage('https://anaudday.github.io/audiyps/boba-pearl.png');
+  mango = loadImage('https://anaudday.github.io/audiyps/mango-bit.png');
+  aloe = loadImage('https://anaudday.github.io/audiyps/aloe-vera.png');
+  pokeball = loadImage('https://anaudday.github.io/audiyps/pokeball.png');
 }
 
 function setup() {
-createCanvas(600, 600);  // canvas size
+createCanvas(windowWidth, windowHeight);  // canvas size
 background(199, 209, 255);   // use our background screen color
+image(glass,500,100);
 
 }
 
@@ -32,73 +34,59 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   // just make sure each key option has the a stroke or fill and then what type of 
   // graphic function
 
- if (toolChoice == '1' ) {  // first tool
+ if (toolChoice == '1' ) {  // house milk tea
    
-    stroke(01);
+    stroke(232, 201, 160);
+    strokeWeight(80);
     line(mouseX, mouseY, pmouseX, pmouseY);
     
-  } else if (toolChoice == '2') { // second tool
+  } else if (toolChoice == '2') { // thai milk tea
 
-    stroke(20);
+    stroke(236, 142, 68);
+    strokeWeight(80);
     line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '3') { // third tool
+  } else if (toolChoice == '3') { // strawberry milk tea
 
-    stroke(300, 100, 0, 80);
+    stroke(250, 212, 230);
     line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '4') {
+  } else if (toolChoice == '4') { // matcha milk tea
 
-    stroke(0, 0, 255);
+    stroke(207, 247, 129);
+    strokeWeight(80);
     line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (key == '5') { // this tool calls a function
-    stroke(0, 0, 255);
-    testbox(20, 20, 200);
-    testbox(200, 20, 20);
-    
-    
-    
-    
-    // make testbox do something!
+  } else if (key == '5') { // rainbow milk tea
+    colorMode(HSB);
+  // background(frameCount % 360, 100, 100);
+   noStroke();
+    stroke((5*frameCount) % 360, 40, 100);
+    fill((5*frameCount) % 360, 100, 100);
+
+    // ellipse(mouseX, mouseY, 30,30);
+    strokeWeight(80);
+    line(mouseX, mouseY, pmouseX, pmouseY);
  //   line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '6') {
+  } else if (toolChoice == '6') { // boba pearl
 
-    stroke(200);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } else if (toolChoice == '7') {
+    image(bobapearl, mouseX, mouseY);
+  } else if (toolChoice == '7') { // mango bit
 
-    fill(100, 200, 100);
-    rect(mouseX, mouseY, 20, 20);
-  } else if (toolChoice == '8') {
+    image(mango, mouseX, mouseY);
+  } else if (toolChoice == '8') { // aloe vera
 
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 20, 20);
-  } else if (toolChoice == '9') {
+    image(aloe, mouseX, mouseY);
+  } else if (toolChoice == '9') { // pokeball
 
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 40, 40);
-  } else if (toolChoice == '0') {
-    stroke(0, 0);
-    fill(random(255), random(255), random(255), random(255));
-    rect(mouseX, mouseY, 200, 150);
-  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX, mouseY);
-    
+    image(pokeball, mouseX, mouseY);
   }
+  
  }
- 
-function testbox(r, g, b) {
-// this is a test function that will show you how you can put your own functions into the sketch
-  x = mouseX;
-  y = mouseY;
-  fill(r, g, b);
-  rect(x-50, y-50, 100, 100);
-
-}
 
 function clear_print() {
 // this will do one of two things, x clears the screen by resetting the background
 // p calls the routine saveme, which saves a copy of the screen
   if (key == 'x' || key == 'X') {
-    background(screenbg); // set the screen back to the background color
+    background(199, 209, 255); // set the screen back to the background color
+    image(glass,500,100);
   } else if (key == 'p' || key == 'P') {
      saveme();  // call saveme which saves an image of the screen
   }
@@ -107,10 +95,9 @@ function clear_print() {
 function saveme(){
     //this will save the name as the intials, date, time and a millis counting number.
     // it will always be larger in value then the last one.
-  filename=initials+day() + hour() + minute() +second();
+  filename=boba+month()+day()+year();
   if (second()!=lastscreenshot) { // don't take a screenshot if you just took one
     saveCanvas(filename, 'jpg');
   }
   lastscreenshot=second(); // set this to the current second so no more than one per second
-  
 }
